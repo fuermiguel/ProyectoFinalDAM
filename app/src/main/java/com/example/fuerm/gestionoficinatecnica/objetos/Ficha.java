b20291c7a2dfe1b97861b2f6b9109cc895ec07f9
fuermiguel <fuermiguel@yahoo.es>
2017-05-03 20:31:17 +0100

    ficheros de test de los objetos

--- app/src/main/java/com/example/fuerm/gestionoficinatecnica/objetos/Ficha.java
+++ app/src/main/java/com/example/fuerm/gestionoficinatecnica/objetos/Ficha.java
@@ -1,5 +1,8 @@
 package com.example.fuerm.gestionoficinatecnica.objetos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by fuerm on 27/04/2017.
  */
@@ -11,52 +14,163 @@ public class Ficha {
     private boolean accesible_discapacitados;
     private String tipo_evento;
     private String horario;
-    private Ambulancias ambulancia; //todo pensar en un arraylist de ambulancias
+    private int numero_ambulancias;
+    private Ambulancia ambulancia ;
+    private List<Ambulancia> ambulancias;
     private boolean venta_alcohol;
     private int numero_aseos;
-    private Aseos aseos;// todo idem
+    private Aseo aseo;
+    private List<Aseo> aseos;
     private boolean cortar_calles;
     private int numero_inchables;
-    private Hinchables hinchables;// todo lo mismo que con las ambulancias
+    private Hinchable hinchable;
+    private List<Hinchable> hinchables;
     private int numero_carpas;
-    private Carpas carpas;//todo lo mismo que con los hinchables
+    private Carpa carpa;
+    private List<Carpa> carpas;
 
     public Ficha() {
+        numero_ambulancias = 0;
+        numero_aseos = 0;
+        numero_carpas = 0;
+        numero_inchables = 0;
+
+        ambulancias = new ArrayList<Ambulancia>();
+        aseos  = new ArrayList<Aseo>();
+        hinchables = new ArrayList<Hinchable>();
+        carpas = new ArrayList<Carpa>();
+
     }
 
-    public Ficha(String localizacion, int numero_participantes, boolean presencia_menores,
-                 boolean accesible_discapacitados, String tipo_evento, String horario,
-                 Ambulancias ambulancia, boolean venta_alcohol, int numero_aseos, Aseos aseos,
-                 boolean cortar_calles, int numero_inchables, Hinchables hinchables,
-                 int numero_carpas, Carpas carpas) {
+    class Medidas {
+        private int alto;
+        private int ancho;
+        private int profundo;
 
-        this.localizacion = localizacion;
-        this.numero_participantes = numero_participantes;
-        this.presencia_menores = presencia_menores;
-        this.accesible_discapacitados = accesible_discapacitados;
-        this.tipo_evento = tipo_evento;
-        this.horario = horario;
-        this.ambulancia = ambulancia;
-        this.venta_alcohol = venta_alcohol;
-        this.numero_aseos = numero_aseos;
-        this.aseos = aseos;
-        this.cortar_calles = cortar_calles;
-        this.numero_inchables = numero_inchables;
-        this.hinchables = hinchables;
-        this.numero_carpas = numero_carpas;
-        this.carpas = carpas;
-    }
-
-    class Ambulancias{
-        private boolean presencia;
-        private String tipo;
-
-        public Ambulancias() {
+        public int getProfundo() {
+            return profundo;
         }
 
-        public Ambulancias(boolean presencia, String tipo) {
-            this.presencia = presencia;
-            this.tipo = tipo;
+        public void setProfundo(int profundo) {
+            this.profundo = profundo;
+        }
+
+        public int getAncho() {
+            return ancho;
+        }
+
+        public void setAncho(int ancho) {
+            this.ancho = ancho;
+        }
+
+        public int getAlto() {
+            return alto;
+        }
+
+        public void setAlto(int alto) {
+            this.alto = alto;
+        }
+    }
+
+    class DatosResponsable {
+        private String cif;
+        private DireccionResponsable direccionResponsable = new DireccionResponsable();
+        private int telefono_fijo;
+        private int telefono_movil;
+
+        class DireccionResponsable {
+            private String pais;
+            private String provincia;
+            private String municipio;
+            private String calle;
+            private int codigo_postal;
+
+            public DireccionResponsable() {
+            }
+
+            public DireccionResponsable(String pais, String provincia, String municipio, String calle, int codigo_postal) {
+                this.pais = pais;
+                this.provincia = provincia;
+                this.municipio = municipio;
+                this.calle = calle;
+                this.codigo_postal = codigo_postal;
+            }
+
+            public String getPais() {
+                return pais;
+            }
+
+            public void setPais(String pais) {
+                this.pais = pais;
+            }
+
+            public String getProvincia() {
+                return provincia;
+            }
+
+            public void setProvincia(String provincia) {
+                this.provincia = provincia;
+            }
+
+            public String getMunicipio() {
+                return municipio;
+            }
+
+            public void setMunicipio(String municipio) {
+                this.municipio = municipio;
+            }
+
+            public String getCalle() {
+                return calle;
+            }
+
+            public void setCalle(String calle) {
+                this.calle = calle;
+            }
+
+            public int getCodigo_postal() {
+                return codigo_postal;
+            }
+
+            public void setCodigo_postal(int codigo_postal) {
+                this.codigo_postal = codigo_postal;
+            }
+        }
+
+        public DireccionResponsable getDireccionResponsable() {
+            return direccionResponsable;
+        }
+
+        public String getCif() {
+            return cif;
+        }
+
+        public void setCif(String cif) {
+            this.cif = cif;
+        }
+
+        public int getTelefono_fijo() {
+            return telefono_fijo;
+        }
+
+        public void setTelefono_fijo(int telefono_fijo) {
+            this.telefono_fijo = telefono_fijo;
+        }
+
+        public int getTelefono_movil() {
+            return telefono_movil;
+        }
+
+        public void setTelefono_movil(int telefono_movil) {
+            this.telefono_movil = telefono_movil;
+        }
+    }
+
+    class Ambulancia {
+        private String matricula;
+        private String tipo;
+
+        public Ambulancia() {
         }
 
         public String getTipo() {
@@ -67,24 +181,20 @@ public class Ficha {
             this.tipo = tipo;
         }
 
-        public boolean isPresencia() {
-            return presencia;
+        public String getMatricula() {
+            return matricula;
         }
 
-        public void setPresencia(boolean presencia) {
-            this.presencia = presencia;
+        public void setMatricula(String matricula) {
+            this.matricula = matricula;
         }
     }
-    class Aseos{
+
+    class Aseo {
         private String localizacion;
         private String tipo;
 
-        public Aseos() {
-        }
-
-        public Aseos(String localizacion, String tipo) {
-            this.localizacion = localizacion;
-            this.tipo = tipo;
+        public Aseo() {
         }
 
         public String getLocalizacion() {
@@ -103,20 +213,15 @@ public class Ficha {
             this.tipo = tipo;
         }
     }
-    class Hinchables{
+
+    class Hinchable {
         private String tipo;
-        private Medidas medidas;
+        private Medidas medidas = new Medidas();
         private boolean anclajes;
-        private DatosResponsable datosResponsable;
+        private DatosResponsable datosResponsable = new DatosResponsable();
 
-        public Hinchables() {
-        }
 
-        public Hinchables(DatosResponsable datosResponsable, String tipo, Medidas medidas, boolean anclajes) {
-            this.datosResponsable = datosResponsable;
-            this.tipo = tipo;
-            this.medidas = medidas;
-            this.anclajes = anclajes;
+        public Hinchable() {
         }
 
         public String getTipo() {
@@ -125,6 +230,41 @@ public class Ficha {
 
         public void setTipo(String tipo) {
             this.tipo = tipo;
+        }
+
+        public Medidas getMedidas() {
+            return medidas;
+        }
+
+        public boolean isAnclajes() {
+            return anclajes;
+        }
+
+        public void setAnclajes(boolean anclajes) {
+            this.anclajes = anclajes;
+        }
+
+        public DatosResponsable getDatosResponsable() {
+            return datosResponsable;
+        }
+    }
+
+    class Carpa {
+
+        private String modelo;
+        private Medidas medidas = new Medidas();
+        private boolean anclajes;
+        private DatosResponsable datosResponsable = new DatosResponsable();
+
+        public Carpa() {
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
         }
 
         public Medidas getMedidas() {
@@ -147,170 +287,6 @@ public class Ficha {
             return datosResponsable;
         }
 
-        public void setDatosResponsable(DatosResponsable datosResponsable) {
-            this.datosResponsable = datosResponsable;
-        }
-
-        class Medidas{
-            private int alto;
-            private int ancho;
-            private int profundo;
-
-            public int getProfundo() {
-                return profundo;
-            }
-
-            public void setProfundo(int profundo) {
-                this.profundo = profundo;
-            }
-
-            public int getAncho() {
-                return ancho;
-            }
-
-            public void setAncho(int ancho) {
-                this.ancho = ancho;
-            }
-
-            public int getAlto() {
-                return alto;
-            }
-
-            public void setAlto(int alto) {
-                this.alto = alto;
-            }
-        }
-
-        class DatosResponsable{
-
-        }
-    }
-    class Carpas {
-
-        private String modelo;
-        private Medidas medidas;
-        private boolean anclajes;
-        private DatosResponsable datosResponsable;
-        private int telefono_movil;
-        private int telefono_fijo;
-
-        public Carpas() {
-        }
-
-        public Carpas(String modelo, Medidas medidas, boolean anclajes,
-                      DatosResponsable datosResponsable, int telefono_movil, int telefono_fijo) {
-            this.modelo = modelo;
-            this.medidas = medidas;
-            this.anclajes = anclajes;
-            this.datosResponsable = datosResponsable;
-            this.telefono_movil = telefono_movil;
-            this.telefono_fijo = telefono_fijo;
-        }
-
-        class Medidas {
-            private int alto;
-            private int ancho;
-            private int profundo;
-
-            public Medidas() {
-            }
-
-            public Medidas(int alto, int ancho, int profundo) {
-                this.alto = alto;
-                this.ancho = ancho;
-                this.profundo = profundo;
-            }
-
-            public int getAlto() {
-                return alto;
-            }
-
-            public void setAlto(int alto) {
-                this.alto = alto;
-            }
-
-            public int getAncho() {
-                return ancho;
-            }
-
-            public void setAncho(int ancho) {
-                this.ancho = ancho;
-            }
-
-            public int getProfundo() {
-                return profundo;
-            }
-
-            public void setProfundo(int profundo) {
-                this.profundo = profundo;
-            }
-        }
-
-        class DatosResponsable {
-            private String cif;
-            private DireccionResponsable direccionResponsable;
-            private int telefono_fijo;
-            private int telefono_movil;
-
-            class DireccionResponsable {
-                private String pais;
-                private String provincia;
-                private String municipio;
-                private String calle;
-                private int codigo_postal;
-
-                public DireccionResponsable() {
-                }
-
-                public DireccionResponsable(String pais, String provincia, String municipio, String calle, int codigo_postal) {
-                    this.pais = pais;
-                    this.provincia = provincia;
-                    this.municipio = municipio;
-                    this.calle = calle;
-                    this.codigo_postal = codigo_postal;
-                }
-
-                public String getPais() {
-                    return pais;
-                }
-
-                public void setPais(String pais) {
-                    this.pais = pais;
-                }
-
-                public String getProvincia() {
-                    return provincia;
-                }
-
-                public void setProvincia(String provincia) {
-                    this.provincia = provincia;
-                }
-
-                public String getMunicipio() {
-                    return municipio;
-                }
-
-                public void setMunicipio(String municipio) {
-                    this.municipio = municipio;
-                }
-
-                public String getCalle() {
-                    return calle;
-                }
-
-                public void setCalle(String calle) {
-                    this.calle = calle;
-                }
-
-                public int getCodigo_postal() {
-                    return codigo_postal;
-                }
-
-                public void setCodigo_postal(int codigo_postal) {
-                    this.codigo_postal = codigo_postal;
-                }
-            }
-        }
     }
 
 
@@ -362,36 +338,12 @@ public class Ficha {
         this.horario = horario;
     }
 
-    public Ambulancias getAmbulancia() {
-        return ambulancia;
-    }
-
-    public void setAmbulancia(Ambulancias ambulancia) {
-        this.ambulancia = ambulancia;
-    }
-
     public boolean isVenta_alcohol() {
         return venta_alcohol;
     }
 
     public void setVenta_alcohol(boolean venta_alcohol) {
         this.venta_alcohol = venta_alcohol;
-    }
-
-    public int getNumero_aseos() {
-        return numero_aseos;
-    }
-
-    public void setNumero_aseos(int numero_aseos) {
-        this.numero_aseos = numero_aseos;
-    }
-
-    public Aseos getAseos() {
-        return aseos;
-    }
-
-    public void setAseos(Aseos aseos) {
-        this.aseos = aseos;
     }
 
     public boolean isCortar_calles() {
@@ -402,35 +354,70 @@ public class Ficha {
         this.cortar_calles = cortar_calles;
     }
 
+    public int getNumero_ambulancias() {
+        return ambulancias.size();
+    }
+
+    public int getNumero_aseos() {
+        return aseos.size();
+    }
+
     public int getNumero_inchables() {
-        return numero_inchables;
-    }
-
-    public void setNumero_inchables(int numero_inchables) {
-        this.numero_inchables = numero_inchables;
-    }
-
-    public Hinchables getHinchables() {
-        return hinchables;
-    }
-
-    public void setHinchables(Hinchables hinchables) {
-        this.hinchables = hinchables;
+        return hinchables.size();
     }
 
     public int getNumero_carpas() {
-        return numero_carpas;
+        return carpas.size();
     }
 
-    public void setNumero_carpas(int numero_carpas) {
-        this.numero_carpas = numero_carpas;
+    public void setAmbulancia(Ambulancia ambulancia) {
+        ambulancias.add(ambulancia);
     }
 
-    public Carpas getCarpas() {
+    public void setAseo(Aseo aseo) {
+        aseos.add(aseo);
+    }
+
+    public void setCarpa(Carpa carpa) {
+        carpas.add(carpa);
+    }
+
+    public void setHinchable(Hinchable hinchable) {
+        hinchables.add(hinchable);
+    }
+
+    public List<Ambulancia> getAmbulancias() {
+        return ambulancias;
+    }
+
+    public List<Aseo> getAseos() {
+        return aseos;
+    }
+
+    public List<Hinchable> getHinchables() {
+        return hinchables;
+    }
+
+    public List<Carpa> getCarpas() {
         return carpas;
     }
 
-    public void setCarpas(Carpas carpas) {
-        this.carpas = carpas;
+    public Ambulancia getAmbulancia() {
+        return new Ambulancia();
     }
+
+    public Aseo getAseo() {
+        return new Aseo();
+    }
+
+    public Hinchable getHinchable() {
+        return new Hinchable();
+    }
+
+    public Carpa getCarpa() {
+        return new Carpa();
+    }
+
+
 }
+
--- app/src/test/java/com/example/fuerm/gestionoficinatecnica/objetos/FichaTest.java
+++ app/src/test/java/com/example/fuerm/gestionoficinatecnica/objetos/FichaTest.java
@@ -14,35 +14,166 @@ public class FichaTest {
     public void Ficha() {
         Ficha ficha = new Ficha();
 
+        ficha.setLocalizacion("localizacion");
+        ficha.setNumero_participantes(100);
+        ficha.setPresencia_menores(true);
         ficha.setAccesible_discapacitados(true);
-        Ficha.Ambulancias ambulancias = ficha.getAmbulancia();
-        ambulancias.setTipo("medicalizada");
-        ambulancias.setPresencia(true);
+        ficha.setTipo_evento("festivo");
+        ficha.setHorario("de 10:00 a 11:00");
 
-        ficha.setAseos();
-        ficha.setCarpas();
-        ficha.setCortar_calles();
-        ficha.setHinchables();
-        ficha.setHorario();
-        ficha.setLocalizacion();
-        ficha.setNumero_aseos();
-        ficha.setNumero_carpas();
-        ficha.setNumero_inchables();
-        ficha.setNumero_participantes();
-        ficha.setPresencia_menores();
-        ficha.setTipo_evento();
-        ficha.setVenta_alcohol();
+        //Ambulancias
+
+        //00
+        Ficha.Ambulancia ambulancia = ficha.getAmbulancia();
+        ambulancia.setMatricula("0000A");
+        ambulancia.setTipo("medicalizada");
+        ficha.setAmbulancia(ambulancia);
+        //01
+        Ficha.Ambulancia ambulancia1 = ficha.getAmbulancia();
+        ambulancia1.setMatricula("0001A");
+        ambulancia1.setTipo("normal");
+        ficha.setAmbulancia(ambulancia1);
+        //02
+        Ficha.Ambulancia ambulancia2 = ficha.getAmbulancia();
+        ambulancia2.setMatricula("0002A");
+        ambulancia2.setTipo("medicalizada");
+        ficha.setAmbulancia(ambulancia2);
+
+        ficha.setVenta_alcohol(false);
+        //Fin Ambulancias
+
+        //Aseos
+
+        //00
+        Ficha.Aseo aseo = ficha.getAseo();
+        aseo.setTipo("portatil");
+        aseo.setLocalizacion("localizacion00");
+        ficha.setAseo(aseo);
+
+        //01
+        Ficha.Aseo aseo1 = ficha.getAseo();
+        aseo1.setTipo("fijo");
+        aseo1.setLocalizacion("localizacion01");
+        ficha.setAseo(aseo1);
 
 
-        //Una manera de hacer el test (uno a uno)
-        Assert.assertEquals("calle", obra.getDireccionObra().getCalle());
+        ficha.setCortar_calles(true);
+        //Fin Aseos
 
-        //Otra manera de hacer el test
-        Assert.assertThat("pais provincia municipio calle 35600",
-                is(obra.getDireccionObra().getPais() + " " +
-                        obra.getDireccionObra().getProvincia() + " " +
-                        obra.getDireccionObra().getMunicipio() + " " +
-                        obra.getDireccionObra().getCalle() + " " +
-                        obra.getDireccionObra().getCodigo_postal()));
+        //Hinchables
+
+        //00
+        Ficha.Hinchable hinchable = ficha.getHinchable();
+        hinchable.setTipo("tipo00");
+        hinchable.setAnclajes(true);
+        //Datos del responsable
+        Ficha.DatosResponsable datosResponsable = hinchable.getDatosResponsable();
+        //Direccion del responsable
+        Ficha.DatosResponsable.DireccionResponsable direccionResponsable = datosResponsable.getDireccionResponsable();
+        direccionResponsable.setPais("pais");
+        direccionResponsable.setProvincia("provincia");
+        direccionResponsable.setMunicipio("municipio");
+        direccionResponsable.setCalle("calle");
+        direccionResponsable.setCodigo_postal(35600);
+        datosResponsable.setCif("42860010q");
+        datosResponsable.setTelefono_fijo(928888888);
+        datosResponsable.setTelefono_movil(637777777);
+        //Medidas
+        Ficha.Medidas medidas = hinchable.getMedidas();
+        medidas.setAlto(10);
+        medidas.setAncho(10);
+        medidas.setProfundo(10);
+        //Añadimos el hinchable a la lista
+        ficha.setHinchable(hinchable);
+        //Fin Hinchables
+
+        //Carpas
+
+        //00
+        Ficha.Carpa carpa = ficha.getCarpa();
+        carpa.setModelo("modelo");
+        carpa.setAnclajes(true);
+        Ficha.Medidas medidas_carpa = carpa.getMedidas();
+        medidas_carpa.setAlto(20);
+        medidas_carpa.setAncho(20);
+        medidas_carpa.setProfundo(20);
+        //Datos del responsable
+        Ficha.DatosResponsable datosResponsable_carpa = hinchable.getDatosResponsable();
+        //Direccion del responsable
+        Ficha.DatosResponsable.DireccionResponsable  direccionResponsable_carpa = datosResponsable.getDireccionResponsable();
+        direccionResponsable_carpa.setPais("pais");
+        direccionResponsable_carpa.setProvincia("provincia");
+        direccionResponsable_carpa.setMunicipio("municipio");
+        direccionResponsable_carpa.setCalle("calle");
+        direccionResponsable_carpa.setCodigo_postal(35600);
+        datosResponsable_carpa.setCif("42860010q");
+        datosResponsable_carpa.setTelefono_fijo(928888888);
+        datosResponsable_carpa.setTelefono_movil(637777777);
+        //Añadimos Carpa a la lista
+        ficha.setCarpa(carpa);
+        //Fin Carpas
+
+        //**********Comenzamos con los Assert*********
+
+        Assert.assertEquals("localizacion",ficha.getLocalizacion());
+        Assert.assertEquals(100,ficha.getNumero_participantes());
+        Assert.assertEquals(true,ficha.isPresencia_menores());
+        Assert.assertEquals(true,ficha.isAccesible_discapacitados());
+        Assert.assertEquals("festivo",ficha.getTipo_evento());
+        Assert.assertEquals("de 10:00 a 11:00",ficha.getHorario());
+
+
+        //Ambulancias
+        int cnt_ambulancia = 0;
+        for (Ficha.Ambulancia iter_ambulancia : ficha.getAmbulancias()) {
+            switch (cnt_ambulancia) {
+                case 0:
+                    Assert.assertThat("0000A medicalizada" ,
+                            is(iter_ambulancia.getMatricula() + " " +
+                                    iter_ambulancia.getTipo()));
+
+                    break;
+                case 1:
+                    Assert.assertThat("0001A normal" ,
+                            is(iter_ambulancia.getMatricula() + " " +
+                                    iter_ambulancia.getTipo()));
+
+                    break;
+
+                case 2:
+                    Assert.assertThat("0002A medicalizada" ,
+                            is(iter_ambulancia.getMatricula() + " " +
+                                    iter_ambulancia.getTipo()));
+
+            }
+            cnt_ambulancia++;
+        }
+        //Fin Ambulancias
+
+        Assert.assertEquals(false,ficha.isVenta_alcohol());
+
+        //Aseos
+        int cnt_aseo = 0;
+        for (Ficha.Aseo iter_aseo : ficha.getAseos()) {
+            switch (cnt_aseo) {
+                case 0:
+                    Assert.assertThat("portatil localizacion00" ,
+                            is(iter_aseo.getTipo() + " " +
+                                    iter_aseo.getLocalizacion()));
+
+                    break;
+                case 1:
+                    Assert.assertThat("fijo localizacion01" ,
+                            is(iter_aseo.getTipo() + " " +
+                                    iter_aseo.getLocalizacion()));
+
+                    break;
+            }
+            cnt_aseo++;
+        }
+
+        //Hinchables
+
+        //Carpas
     }
 }
